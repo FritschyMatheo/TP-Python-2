dde83b16ffbfccb57c3628a405446e67a2567ca7
Matheo Fritschy <matheo.fritschy@uha.fr>
2023-11-16 10:28:38 +0100

    tp1exo3v2

--- TP1/tp1exo3.py
+++ TP1/tp1exo3.py
@@ -1,13 +1,13 @@
 #Script de calcul du nombre de minutes écoulées dans le mois
 print("Script de calcul du nombre de minutes écoulées dans le mois.")
 print("Saisir le jour du mois:")
-jour=input()
+jours=input()
 print("Saisir les heures:")
 heures=input()
 print("Saisir les minutes:")
 minute=input()
-jour=int(jour)
+jours=int(jours)
 heures=int(heures)
 minute=int(minute)
-min=jour*1440+heures*60+minute
+min=jours*1440+heures*60+minute
 print(f"Depuis le début du mois, il s'est écoulé : {min} minutes !")
