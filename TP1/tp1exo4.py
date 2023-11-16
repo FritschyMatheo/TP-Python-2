dce92454cdff2e6c3d8902e5a796c0feb97db6f7
Matheo Fritschy <matheo.fritschy@uha.fr>
2023-11-16 11:00:36 +0100

    tp1exo4

---
+++ TP1/tp1exo4.py
@@ -0,0 +1,10 @@
+#Script de calcul du nombres de minutes écoulées depuis le début du mois en date
+print("Saisir le nombre de minutes écoulées depuis le début du mois:")
+min=input()
+min=int(min)
+minutes=min%60
+hr=int(min/60)
+heures=hr%24
+jr=int(hr/24)
+jours=jr
+print(minutes, "minutes d'un mois représentent la date du" ,jours, "et l'heure : ",heures,":",minutes)
