802249e01081d5f9a0f535daf543ad080e3ce227
Matheo Fritschy <matheo.fritschy@uha.fr>
2023-11-30 10:45:45 +0100

    fin ex7

---
+++ TP2/tp2exo7.py
@@ -0,0 +1,8 @@
+#Script de simulation du lancement d'une pièce truquée sur pile
+import random
+piece=random.randint(0,2)
+if piece < 1 or piece == 1 :
+    print("Pile !")
+else :
+    print("Face !")
+print(f"{piece}")
