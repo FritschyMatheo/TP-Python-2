668d2c25b31fa1797e2a645b1a4d37744471e4c4
Matheo Fritschy <matheo.fritschy@uha.fr>
2023-11-30 09:04:09 +0100

    fin ex1

--- TP2/tp2exo1.py
+++ TP2/tp2exo1.py
@@ -1,12 +1,10 @@
 #script de permutation de deux variables
-print("Entrez une première valeur X :")
-x=input()
-print("Entrez une seconde valeur Y :")
-y=input()
+x=input("Entrez  x: ")
+y=input("Entrez  y: ")
 x=int(x)
 y=int(y)
-print(f"Avant la permutation X vaut {x} et Y {y}")
+print(f"Avant la permutation \nx : {x}\ny : {y}")
 t=x
 x=y
 y=t
-print(f"Après la permutation, X vaut {x} et Y {y}")
+print(f"Après la permutation \nx : {x}\ny : {y}")
