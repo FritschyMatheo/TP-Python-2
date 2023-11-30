7d312c6759782081cbe2447edc68f8615f1fed21
Matheo Fritschy <matheo.fritschy@uha.fr>
2023-11-30 10:33:28 +0100

    fin ex5

--- TP2/tp2ex5.py
+++ TP2/tp2ex5.py
@@ -1,5 +1,14 @@
 #Script expressions conditionnelles
 n=input("Entrez un nombre entier: ")
-if 
-
-#pair positif   pair négatif    zéro impair     impair positif  impair négatif
+n=int(n)
+if n == 0 :
+    print("Le nombre est zéro (et il est pair)")
+elif n%2 == 0 :
+    if n>0 :
+        print("Le nombre est positif et pair")
+    else :
+        print("Le nombre est négatif et pair")
+elif n>0 :
+    print("Le nombre est positif et impair")
+else :
+    print("Le nombre est négatif et impair")
