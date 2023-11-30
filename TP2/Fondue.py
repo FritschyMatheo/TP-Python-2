941de4f32c08359195680f5a160c6d1c07c160bf
Matheo Fritschy <matheo.fritschy@uha.fr>
2023-11-30 09:50:30 +0100

    fin ex4

--- TP2/Fondue.py
+++ TP2/Fondue.py
@@ -5,7 +5,10 @@ eau=2           #Quantité d'eau pour la recette de base (en decilitres)
 ail=2           #Quantité d'ail pour la recette de base (en gousses)
 pain=400        #Quantité de pain pour la recette de base (en grammes)
 nbConvives=input("Entrez le nombre de personne(s) conviées à la fondue : ")
+nbConvives=int(nbConvives)
+fromageQuantite= fromage * nbConvives / BASE
+eauQuantite= eau * nbConvives / BASE
+ailQuantite= ail * nbConvives / BASE
+painQuantite= pain * nbConvives / BASE
 
-
-
-print(f"Pour faire une fondue fribourgeoise pour {nbConvives} personnes, il vous faut :\n- {fromage} gr de fromage\n- {eau} dl d'eau\n- {ail} gousse(s) d'ail\n- {pain} gr de pain")
+print(f"Pour faire une fondue fribourgeoise pour {nbConvives} personnes, il vous faut :\n- {fromageQuantite} gr de fromage\n- {eauQuantite} dl d'eau\n- {ailQuantite} gousse(s) d'ail\n- {painQuantite} gr de pain")
