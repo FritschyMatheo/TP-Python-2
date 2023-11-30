30c7ad6a5201eea4fd392a37daed45ec60129f3b
Matheo Fritschy <matheo.fritschy@uha.fr>
2023-11-30 09:18:00 +0100

    fin ex3

--- TP2/TP2_swap.py
+++ TP2/TP2_swap.py
@@ -4,15 +4,8 @@ c=input("Entrez la troisieme valeur : ")
 
 print("Les valeurs entrees sont : a = " + a + ", b = " + b + " et c = " + c)
 print("Permutation: a ==> b, b ==> c, c ==> a")
-"""      *******************************************
-         * Completez le programme a partir d'ici.
-         *******************************************
-"""
-
-
-"""     *******************************************
-         * Ne rien modifier apres cette ligne.
-         *******************************************
-"""
-
+t=b
+b=a
+a=c
+c=t
 print("Les valeurs permutees sont : a = " + a + ", b = " + b + " et c = " + c)
