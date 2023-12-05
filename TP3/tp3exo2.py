55e589f11c7ba85093811e4a4d41778941f6e3e1
Matheo Fritschy <matheo.fritschy@uha.fr>
2023-12-05 10:59:08 +0100

    Fin ex2v2

--- TP3/tp3exo2.py
+++ TP3/tp3exo2.py
@@ -1,9 +1,18 @@
 #Affichage des nombres par ordre décroissant
+import time
+
+#Version for
+print("Boucle for")
+n=int(input("Entrez un nombre entier positif: "))
+for i in range(n,-1,-1):
+    print(f"{n}")
+    n-=1
+    time.sleep(1)
 
 #Version while
-import time
+print("Boucle while")
 n=int(input("Entrez un nombre entier positif: "))
-while n>-1:
+while n>=0:
     print(f"{n}")
     n-=1
     time.sleep(1)
