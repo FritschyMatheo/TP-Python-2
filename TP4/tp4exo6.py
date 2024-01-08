00738732fd225b0d8993219b22c22b3aad134384
Matheo Fritschy <matheo.fritschy@uha.fr>
2024-01-08 16:52:35 +0100

    Tri de nombres

---
+++ TP4/tp4exo6.py
@@ -0,0 +1,18 @@
+#Script de tri de N nombres
+
+tab=[5, 2, 4, 8, 1, 3]
+print(tab)
+for i in range(len(tab) - 1):
+    min=tab[i]
+    for j in range(i+1, len(tab)):
+        if tab[j] < min:
+            min=tab[j]
+            index=j
+    if min < tab[i]:
+        temp=tab[i]
+        tab[i]=tab[index]
+        tab[index]=temp
+    print(tab)
+print(f"Trié avec sorted : {sorted(tab)}")
+tab.sort()
+print(f"Trié avec sort: {tab}")
